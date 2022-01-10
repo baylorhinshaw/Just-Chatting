655585fb6aca8d9ca1db1ffea2f56ce103a0aa1a
Baylor <baylorhinshaw21@gmail.com>
2022-01-09 22:22:07 -0500

    was missing a this. got thoughts to work now

--- models/thought.js
+++ models/thought.js
@@ -37,7 +37,7 @@ const thoughtSchema = new Schema(
 thoughtSchema
         .virtual("reactionCount")
         .get(function () {
-            return reactionCount.length;
+            return this.reactions.length;
         });
   
 const Thought = model("thought", thoughtSchema);
